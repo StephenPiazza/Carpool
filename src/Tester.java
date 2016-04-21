843e2584c8ba9ce87e85906d31a3d581d8066239
Wansh B Malekoo <wansh_malekoo@hotmail.com>
2016-04-21 13:34:47 -0700

    test commit

--- src/Tester.java
+++ src/Tester.java
@@ -5,5 +5,7 @@ public class Tester {
 	public static void main(String[] args) {
 		MainControllerGUI mainview = new MainControllerGUI();
 		mainview.setVisible(true);
+                
+                //test comment
 	}
 }
